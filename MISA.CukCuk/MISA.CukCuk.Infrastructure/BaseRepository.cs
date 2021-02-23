d1621466f200cb90251d4d7ca63fc717819cf443
talongkhanh <longkhanh02092000@gmail.com>
2021-02-23 11:32:53 +0700

    update

--- MISA.CukCuk/MISA.CukCuk.Infrastructure/BaseRepository.cs
+++ MISA.CukCuk/MISA.CukCuk.Infrastructure/BaseRepository.cs
@@ -40,52 +40,93 @@ namespace MISA.CukCuk.Infrastructure
         #region Method
         public IEnumerable<T> Get(string sqlcommand = null, object parameters = null, CommandType commandType = CommandType.Text)
         {
+            try
+            {
+                if (sqlcommand == null)
+                    return _dbConnection.Query<T>($"Select * from {_className}");
+                return _dbConnection.Query<T>(sqlcommand, parameters, commandType: commandType);
+            }
+            catch (Exception)
+            {
 
-            if (sqlcommand == null)
-                return _dbConnection.Query<T>($"Select * from {_className}");
-            return _dbConnection.Query<T>(sqlcommand, parameters, commandType: commandType);
+                throw;
+            }
+            
 
         }
 
         public T GetById(string id)
         {
-            var _params = new DynamicParameters();
-            _params.Add($"{_className}Id", id);
-            return _dbConnection.Query<T>($"Select * from {_className} Where {_className}Id = @{_className}Id", _params).FirstOrDefault();
+            try
+            {
+                var _params = new DynamicParameters();
+                _params.Add($"{_className}Id", id);
+                return _dbConnection.Query<T>($"Select * from {_className} Where {_className}Id = @{_className}Id", _params).FirstOrDefault();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
         }
 
         public int Insert(T entity)
         {
-            var res = _dbConnection.Execute(
+            try
+            {
+                var res = _dbConnection.Execute(
                 $"Proc_Insert{_className}",
                 MappingParams(entity),
                 commandType: CommandType.StoredProcedure
                 );
-            return res;
+                return res;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public int Update(string id, T entity)
         {
-            var _params = MappingParams(entity);
-            _params.Add($"@{_className}Id", id);
-            var res = _dbConnection.Execute(
-                $"Proc_Update{_className}",
-                _params,
-                commandType: CommandType.StoredProcedure
-                );
-            return res;
+            try
+            {
+                var _params = MappingParams(entity);
+                _params.Add($"@{_className}Id", id);
+                var res = _dbConnection.Execute(
+                    $"Proc_Update{_className}",
+                    _params,
+                    commandType: CommandType.StoredProcedure
+                    );
+                return res;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public int Delete(string id)
         {
-            var _param = new DynamicParameters();
-            _param.Add($"{_className}Id", id);
-            var res = _dbConnection.Execute(
-                $"Proc_Delete{_className}",
-                _param,
-                commandType: CommandType.StoredProcedure
-                );
-            return res;
+            try
+            {
+                var _param = new DynamicParameters();
+                _param.Add($"{_className}Id", id);
+                var res = _dbConnection.Execute(
+                    $"Proc_Delete{_className}",
+                    _param,
+                    commandType: CommandType.StoredProcedure
+                    );
+                return res;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         /// <summary>
